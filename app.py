3a110e6cc2c3ea49f7b99ccf440934122dab6e1d
Guilherme <guilhermesergiooliveira55@gmail.com>
2020-04-14 21:37:27 -0300

    Quase terminando

--- app.py
+++ app.py
@@ -7,9 +7,62 @@ def sidebar_configs():
     style = st.sidebar.selectbox("Chose a seaborn grid: ", ("darkgrid", "whitegrid", "dark", "white", "ticks"))
     sns.set_style(style)
 
+def plot_graph(df):
+        graph = st.selectbox("Chose the graph: ", ("Select", "Bar", "Histogram", "Scatter", "Heatmap"))
+        if(graph == "Bar"):
+            columns = st.multiselect("Chose the columns: ", df.columns)
+            if(len(columns) == 1):
+                sns.countplot(x= columns[0], data = df)
+                st.pyplot()
+            elif(len(columns) > 1):
+                sns.barplot(x=columns[0], y=columns[1], data=df)
+                st.pyplot()
+        if(graph == "Histogram"):
+            column = st.selectbox("Chose the column: ", df.columns)
+            if(column):
+                sns.distplot(df[column])
+                st.pyplot()
+        if(graph == "Scatter"):
+            columns = st.multiselect("Chose the columns: ", df.columns)
+            if(len(columns) > 1):
+                sns.scatterplot(x=columns[0], y=columns[1], data=df)
+                st.pyplot()
+
+def drop_useless_columns(df):
+    columns = st.multiselect("Chose the columns", df.columns)
+    button = st.button("Drop")
+    if button:
+        new_df = df.drop(columns, axis = 1)
+        st.markdown("New dataset")
+        st.dataframe(new_df)
+        return new_df
+
+def complet_missing_data(df):
+    select_method = st.selectbox("Chose the method: ", ("Select", "Mean", "Median", "Mode"))
+    if(select_method == "Mean"): 
+        columns =  st.multiselect("Chose the columns: ", df.columns)
+        for column in columns:
+            df[column] = df[column].fillna(df[column].mean())
+        values_explore = pd.DataFrame({"Column": df.columns, "Types": df.dtypes, "NA #": df.isna().sum(), "NA %": df.isna().sum()/df.shape[0] * 100})
+        st.dataframe(values_explore)
+        
+    if(select_method == "Median"): 
+        columns =  st.multiselect("Chose the columns: ", df.columns)
+        for column in columns:
+            df[column] = df[column].fillna(df[column].median())
+        values_explore = pd.DataFrame({"Column": df.columns, "Types": df.dtypes, "NA #": df.isna().sum(), "NA %": df.isna().sum()/df.shape[0] * 100})
+        st.dataframe(values_explore)
+    
+    if(select_method == "Mode"): 
+        columns =  st.multiselect("Chose the columns: ", df.columns)
+        for column in columns:
+            df[column] = df[column].fillna(df[column].mode())
+        values_explore = pd.DataFrame({"Column": df.columns, "Types": df.dtypes, "NA #": df.isna().sum(), "NA %": df.isna().sum()/df.shape[0] * 100})
+        st.dataframe(values_explore)
+
 def main():
     sidebar_configs()
-    st.title("Analyzing datas with Streamlit")
+    st.title("Analyzing data with Streamlit")
     st.image("image/image.jpg", use_column_width=True)
     file = st.file_uploader("Upload your file (csv) ", type="csv")
     if file is not None:
@@ -24,34 +77,23 @@ def main():
         values_explore = pd.DataFrame({"Column": df.columns, "Types": df.dtypes, "NA #": df.isna().sum(), "NA %": df.isna().sum()/df.shape[0] * 100})
         st.subheader("Info about missing data and data types")
         st.dataframe(values_explore)
+        
         st.subheader("Graphs")
-        checkbox_flag = st.checkbox("Plot a graph?") 
+        checkbox_flag = st.checkbox("Plot a graph") 
         if(checkbox_flag):
-            graph = st.selectbox("Chose the graph: ", ("Bar", "Histogram", "Scatter, Heatmap"))
-            if(graph == "Bar"):
-                columns = st.multiselect("Chose the columns: ", df.columns)
-                if(len(columns) == 1):
-                    sns.countplot(x= columns[0], data = df)
-                    st.pyplot()
-                elif(len(columns) > 1):
-                    sns.barplot(x=columns[0], y=columns[1], data=df)
-                    st.pyplot()
-            if(graph == "Histogram"):
-                column = st.selectbox("Chose the column: ", df.columns)
-                if(column):
-                    sns.distplot(df[column])
-                    st.pyplot()
-            if(graph == "Scatter"):
-                columns = st.multiselect("Chose the columns: ", df.columns)
-                if(len(columns) > 1):
-                    sns.scatterplot(x=columns[0], y=columns[1], data=df)
-                    st.pyplot()
-            if(graph == "Heatmap"):
-                sns.heatmap()
-                
-                                
-                    
+            plot_graph(df)
 
+        st.subheader("Drop useles columns")
+        drop_flag = st.checkbox("Drop columns")
+        if(drop_flag):
+            new_df = drop_useless_columns(df) 
+        else:
+            new_df = df
+        
+        st.subheader("Complete missing data")
+        complete_flag = st.checkbox("Complete columns")
+        if(complete_flag):
+            complet_missing_data(new_df)        
 
 
 if __name__ == "__main__":
