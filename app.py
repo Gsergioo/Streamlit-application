91e323cb29a243663017f50a435c068470752297
Guilherme <guilhermesergiooliveira55@gmail.com>
2020-04-22 16:57:36 -0300

    Adição de funções

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns 
 import base64
-
+import matplotlib.pyplot as plt
 
 def get_table_download_link(df):
     """Generates a link allowing the data in a given panda dataframe to be downloaded
@@ -13,6 +13,7 @@ def get_table_download_link(df):
     b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
     href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
     return href
+
 def show_basic_infos(df):
     st.header("Basic infos about data")
     num_lines = st.slider("Enter a number of lines to visualize: ", 1, 100)
@@ -50,7 +51,7 @@ def show_descriptive_info(df):
             st.markdown("Description: ")
             st.write(df[columns].describe())
     else:
-        info = st.selectbox("Chose method: ", ("Select", "Mean", "Median", "Standar Deviation", "Quartiles", "Complete Description"))
+        info = st.selectbox("Choose method: ", ("Select", "Mean", "Median", "Standar Deviation", "Quartiles", "Complete Description"))
         if(info == "Mean"):
             st.markdown("Mean: ")
             st.write(df.mean())
@@ -69,27 +70,22 @@ def show_descriptive_info(df):
             st.markdown("Description: ")
             st.write(df.describe())
 
-def sidebar_configs():
-    st.sidebar.title("Let's go!")
-    data_option = st.sidebar.selectbox("Chose dataset: ", ("Load file", "Use example"))
-    if(data_option == "Load file"):
-        file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
-    if(file is not None):
-        df = pd.read_csv(file)
-        st.sidebar.subheader("Style configuration")
-        style = st.sidebar.selectbox("Chose a seaborn grid: ", ("darkgrid", "whitegrid", "dark", "white", "ticks"))
-        sns.set_style(style)
+def show_correlations_info(df):
+    st.header("Data correlations")
+    options = [x for x in df.columns if(df[x].dtypes != "O" and df[x].dtypes != "str" and df[x].dtypes != "bool")]
+    columns = st.multiselect("Select columns: ", options)
+    correlation_type = st.selectbox("Choose correlation type: ", ("Select", "Pearson", "Spearman"))
+    if(columns is not None):
+        if(correlation_type == "Pearson"):
+            st.markdown("Correlation: ")
+            st.write(df[columns].corr(method = "pearson"))
 
-        basic_infos = st.sidebar.checkbox("Informações básicas")
-        if(basic_infos):
-            show_basic_infos(df)
-
-        univariate_info = st.sidebar.checkbox("Informações descritivas")
-        if(univariate_info):
-            show_descriptive_info(df)    
-
+        if(correlation_type == "Spearman"):
+            st.markdown("Correlation: ")
+            st.write(df[columns].corr(method = "spearman"))
+        
 def plot_graph(df):
-        graph = st.selectbox("Chose the graph: ", ("Select", "Bar", "Histogram", "Scatter", "Heatmap"))
+        graph = st.selectbox("Choose the graph: ", ("Select", "Bar", "Histogram", "Scatter", "Heatmap"))
         if(graph == "Bar"):
             columns = st.multiselect("Chose the columns: ", df.columns)
             if(len(columns) == 1):
@@ -99,21 +95,27 @@ def plot_graph(df):
                 sns.barplot(x=columns[0], y=columns[1], data=df)
                 st.pyplot()
         if(graph == "Histogram"):
-            column = st.selectbox("Chose the column: ", df.columns)
+            column = st.selectbox("Choose the column: ", df.columns)
+            bins = st.slider("Select a number of bins: ", 1, 100)
             if(column):
-                sns.distplot(df[column])
+                sns.distplot(df[column], bins = bins)
                 st.pyplot()
         if(graph == "Scatter"):
-            columns = st.multiselect("Chose the columns: ", df.columns)
+            columns = st.multiselect("Choose the columns: ", df.columns)
             if(len(columns) > 1):
                 sns.scatterplot(x=columns[0], y=columns[1], data=df)
                 st.pyplot()
+        if(graph == "Heatmap"):
+            options = [x for x in df.columns if(df[x].dtypes != "O" and df[x].dtypes != "str" and df[x].dtypes != "bool")]
+            plt.figure(figsize=(14,12))
+            sns.heatmap(df.corr())
+            st.pyplot()
 
 
 def complet_missing_data(df):
-    select_method = st.selectbox("Chose the method: ", ("Mean", "Median", "Mode"))
+    select_method = st.selectbox("Choose the method: ", ("Mean", "Median", "Mode"))
     if(select_method == "Mean"): 
-        columns =  st.multiselect("Chose the columns: ", df.columns)
+        columns =  st.multiselect("Choose the columns: ", df.columns)
         for column in columns:
             df[column] = df[column].fillna(df[column].mean())
         values_explore = pd.DataFrame({"Column": df.columns, "Types": df.dtypes, "NA #": df.isna().sum(), "NA %": df.isna().sum()/df.shape[0] * 100})
@@ -121,7 +123,7 @@ def complet_missing_data(df):
         return df
         
     if(select_method == "Median"): 
-        columns =  st.multiselect("Chose the columns: ", df.columns)
+        columns =  st.multiselect("Choose the columns: ", df.columns)
         for column in columns:
             df[column] = df[column].fillna(df[column].median())
         values_explore = pd.DataFrame({"Column": df.columns, "Types": df.dtypes, "NA #": df.isna().sum(), "NA %": df.isna().sum()/df.shape[0] * 100})
@@ -129,17 +131,48 @@ def complet_missing_data(df):
         return df 
       
     if(select_method == "Mode"): 
-        columns =  st.multiselect("Chose the columns: ", df.columns)
+        columns =  st.multiselect("Choose the columns: ", df.columns)
         for column in columns:
             df[column] = df[column].fillna(df[column].mode())
         values_explore = pd.DataFrame({"Column": df.columns, "Types": df.dtypes, "NA #": df.isna().sum(), "NA %": df.isna().sum()/df.shape[0] * 100})
         st.dataframe(values_explore)
         return df
 
+def sidebar_configs():
+    df = None
+    st.sidebar.title("Let's go!")
+    data_option = st.sidebar.selectbox("Choose dataset: ", ("Load file", "Use example (iris dataset)"))
+    if(data_option == "Load file"):
+        file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
+        if(file is not None):
+            df = pd.read_csv(file)
+    else:
+        df = pd.read_csv("dataset/iris.csv")
+    if(df is not None):
+        style = st.sidebar.selectbox("Choose a seaborn grid: ", ("darkgrid", "whitegrid", "dark", "white", "ticks"))
+        sns.set_style(style)
+
+        basic_infos = st.sidebar.checkbox("Basic infos")
+        if(basic_infos):
+            show_basic_infos(df)
+
+        univariate_info = st.sidebar.checkbox("Descriptive infos")
+        if(univariate_info):
+            show_descriptive_info(df)    
+        
+        correlations_info = st.sidebar.checkbox("Correlations infos")
+        if(correlations_info):
+            show_correlations_info(df)
+        
+        plot_graph_flag = st.sidebar.checkbox("Plot graphs")
+        if(plot_graph_flag):
+            plot_graph(df)
+
 def main():
     st.title("Analyzing data with Streamlit")
     st.image("image/image.jpg", use_column_width=True)
     sidebar_configs()
+    st.write("Desenvolvido no aceleradev da Codenation")
 
 if __name__ == "__main__":
     main()
